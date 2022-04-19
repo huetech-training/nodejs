0cb9efceba719a688fcced34a7a2c84440364350
Debanil Guha <debanil@hutechsolutions.com>
2022-04-19 09:16:57 +0530

    Changes

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -13,4 +13,8 @@ router.post('/add', (req, res) => {
   res.send('<h2>Add Product</h2>');
 });
 
+router.get('/add', (req, res) => {
+  res.render(path.join(__dirname, '../views', 'products-form.ejs'));
+});
+
 module.exports = router;
