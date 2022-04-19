e9131be15d85536d8a490f25d8456f5dc1e3ce31
Kanchan Karjee <kanchan.karjee@gmail.com>
2022-04-19 08:00:21 +0530

    cleanup

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,5 @@
 const express = require('express');
 
 const router = express.Router();
-router.post('/login', (req, res) => {
-  res.send('<h2>Sign In</h2>');
-});
-
-router.post('/register', (req, res) => {
-  res.send('<h2>Register</h2>');
-});
 
 module.exports = router;
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,5 @@
 const express = require('express');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.send('<h2>Products</h2>');
-});
-router.post('/add', (req, res) => { res.send('<h2>Add Product</h2>'); });
 
 module.exports = router;
